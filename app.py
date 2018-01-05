1e1f884ee25f44fc89be062edf53c1a9b574ecd4
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2018-01-05 10:06:42 -0500

    added thank you in converse

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Author: Daniel Gisolfi
 #Date: 1/3/18
 #GroupMe Chatbot
-#v111
+#v106
 
 import os
 import json
@@ -56,8 +56,7 @@ def methodController(data):
 
 	if "marty" in words:
 		if greetings in words:
-			greetReply(data, greetings)
-
+			greetReply(data)
 		elif"echo" in words: 
 			echo(data)
 		elif "create" in words:
@@ -96,15 +95,28 @@ def converse(data):
 		["Who made you?",
 		"Daniel Gisolfi"])
 
+	bot.train(
+		["Thank you",
+		"Your Welcome"])
 
 	# bot.train("chatterbot.corpus.english");
 	
 	msg = bot.get_response(data['text'])
 	sendMessage(msg)
 
-def greetReply(data, greetings):
+def greetReply(data):
 	name = re.sub("[^\w]", " ",  data['name']).split()
 	firstName = name[0]
+
+	greetings = [
+	"Hello",
+	"Hey",
+	"Hi there",
+	"Hi",
+	"Hallo"
+	"Cheerio",
+	"Cheers"
+	]
 
 	#Get the number of elesments in the list
 	maxLen = len(greetings)
